b4af0816a9ebedfafcfbad42dcd1ac649abbf904
puneetsingh99 <puneetsingh@github.com>
2020-12-07 09:55:25 +0530

    solved bug
    solved bug in app.js

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-var input = document.querySelector("#txt-input");
-var inputText = input.value;
-
 
 var translate = document.querySelector("#btn-translate");
 
 var output = document.querySelector("#txt-output");
-
-var serverUrl = "https://api.funtranslations.com/translate/minion.json"
-const url = serverUrl + "?"+"text="+inputText; 
-
 
 function errorHandler(error){
     console.log("error: ", error);
@@ -18,6 +11,10 @@ function errorHandler(error){
 
 
 function translator(){
+    var input = document.querySelector("#txt-input");
+    var inputText = input.value;
+    var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+    const url = serverUrl + "?"+"text="+inputText; 
     fetch(url)
     .then(response => response.json())
     .then(json => output.value = json.contents.translated)
